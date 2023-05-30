aea21a2b214addfe2268c642a4452889bb2912a4
David <dryan10101@gmail.com>
2023-05-30 10:55:29 -0700

    wrote basic outline of Early3.java

---
+++ Early3.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+// This version of Early advances the concept a bit further in terms of automation.  Still just doing linear regression.
+// Goal 1: Create randomized sets of data to skip manual data creation
+// Goal 2: Build a system that can handle any number of features / weights
+
+public class Early3 {
+    
+    Early3() {
+        // Data creation
+        
+
+        // Initialize model system
+
+    }
+
+    public static void main(String[] args) {
+        Early3 t = new Early3();
+        t.model();
+    }
+}
