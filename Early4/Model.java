7e2a10972027d4f8c959ddfb67db81085df4fce1
David <dryan10101@gmail.com>
2023-06-13 10:56:34 -0700

    basic attributes and constructor for Model.java

--- Early4/Model.java
+++ Early4/Model.java
@@ -1,4 +1,19 @@
 package Early4;
+
+import java.util.ArrayList;
+
 public class Model {
+    private ArrayList<Double> weights;
+    private Double bias;
+
+    Model(int numOfWeights) {
+        this.weights = new ArrayList<Double>();
+        for (int i = 0; i < numOfWeights; i++) {
+            this.weights.add(0.0);
+        }
+        this.bias = 0.0;
+    }
+
     
+
 }
