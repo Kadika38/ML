250879f23e30f1998f66584f0752df69453603f0
David <dryan10101@gmail.com>
2023-06-13 11:08:20 -0700

    added basic attributes and constructor to Dataset.java

--- Early4/Dataset.java
+++ Early4/Dataset.java
@@ -1,5 +1,15 @@
 package Early4;
 
+import java.util.ArrayList;
+
 public class Dataset {
+    private ArrayList<ArrayList<Double>> dataset;
+    private ArrayList<String> features;
+
+    Dataset(ArrayList<ArrayList<Double>> dataset, ArrayList<String> features) {
+        this.dataset = dataset;
+        this.features = features;
+    }
+
     
 }
