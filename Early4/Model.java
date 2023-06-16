2a558bcc2cd0761e02f5533658ae135d86f2eb6b
David <dryan10101@gmail.com>
2023-06-16 09:06:42 -0700

    Moved linear regression process into the Model class as Model.linearRegression(), adjusted that process accordingly, and cleared that part from Modeler.model()

--- Early4/Model.java
+++ Early4/Model.java
@@ -5,28 +5,74 @@ import java.util.ArrayList;
 public class Model {
     private ArrayList<Double> weights;
     private Double bias;
+    private Dataset dataset;
 
-    Model(int numOfWeights) {
+    Model(Dataset dataset) {
         this.weights = new ArrayList<Double>();
-        for (int i = 0; i < numOfWeights; i++) {
+        for (int i = 0; i < dataset.getNumOfFeatures(); i++) {
             this.weights.add(0.0);
         }
         this.bias = 0.0;
+        this.dataset = dataset;
     }
 
-    public ArrayList<Double> getWeights() {
-        return this.weights;
-    }
+    // Performs one iteration of linear regression on the dataset, adjusting the model accordingly
+    // Returns the squared average loss of the new model when compared to the data
+    public Double linearRegression(Double lr) {
+        // Compute loss for each feature and adjust model weight accordingly
+            for (int i = 0; i < this.weights.size(); i++) {
+                ArrayList<Double> current = this.dataset.getSingleFeatureDataset(i);
+                Double totalLoss = 0.0;
+                for (int j = 0; j < this.dataset.getNumOfDatapoints(); j++) {
+                    Double holder = 0.0;
+                    Double coefficient = (-2.0) * current.get(j);
+                    Double label = this.dataset.getLabels().get(j);
+                    for (int f = 0; f < this.weights.size(); f++) {
+                        ArrayList<Double> weightAdjCurrent = this.dataset.getSingleFeatureDataset(f);
+                        holder += (this.weights.get(f) * weightAdjCurrent.get(j));
+                    }
+                    holder += this.bias;
+                    totalLoss += coefficient * (label - holder);
+                }
+                Double lossAdj = totalLoss / this.dataset.getNumOfDatapoints();
+                // Adjust model weight
+                this.weights.set(i, this.weights.get(i) - (lr * lossAdj));
+            }
 
-    public Double getBias() {
-        return this.bias;
-    }
+            // perform same process as above, but for model bias
+            Double totalLossB = 0.0;
+            for (int i = 0; i < this.dataset.getNumOfDatapoints(); i++) {
+                Double holder = 0.0;
+                Double coefficient = (-2.0);
+                Double label = this.dataset.getLabels().get(i);
+                for (int j = 0; j < this.weights.size(); j++) {
+                    ArrayList<Double> current = this.dataset.getSingleFeatureDataset(j);
+                    holder += (this.weights.get(j) * current.get(i));
+                }
+                holder += this.bias;
+                totalLossB += coefficient * (label - holder);
+            }
+            Double lossAdjB = totalLossB / this.dataset.getNumOfDatapoints();
+            this.bias = (this.bias - (lr * lossAdjB));
 
-    public void setWeight(int index, Double value) {
-        this.weights.set(index, value);
-    }
+            System.out.print("Weights n Bias: ");
+            for (int i = 0; i < this.weights.size(); i++) {
+                System.out.print("w" + i + ":" + this.weights.get(i) + " ");
+            }
+            System.out.println("b:" + this.bias);
 
-    public void setBias(Double value) {
-        this.bias = value;
+            // compute average loss and return it
+            Double averageLoss = 0.0;
+            for (int i = 0; i < this.dataset.getNumOfDatapoints(); i++) {
+                Double predictedValue = this.bias;
+                for (int j = 0; j < this.weights.size(); j++) {
+                    ArrayList<Double> current = dataset.getSingleFeatureDataset(j);
+                    predictedValue += (this.weights.get(j) * current.get(i));
+                }
+                averageLoss += this.dataset.getLabels().get(i) - predictedValue;
+            }
+            averageLoss = averageLoss / this.dataset.getNumOfDatapoints();
+            System.out.println("Average Loss: " + averageLoss);
+            return Math.pow(averageLoss, 2);
     }
 }
--- Early4/Modeler.java
+++ Early4/Modeler.java
@@ -9,91 +9,17 @@ public class Modeler {
     }
 
     public Model model(Dataset dataset) {
-        // initialize and set weights and bias to 0
-        int numOfFeatures = dataset.getNumOfFeatures();
-        Model model = new Model(numOfFeatures);
-        for (int i = 0; i < numOfFeatures; i++) {
-            model.setWeight(i, 0.0);
-        }
-        model.setBias(0.0);
+        // initialize the model
+        Model model = new Model(dataset);
 
         Double lr = 0.01;
         Double lrReductionRate = 0.1;
         Double averageLossPrevious = 0.0;
-        Double averageLossNew = -10000.0;
+        Double averageLossNew = null;
         int epochs = 0;
 
-        while (averageLossPrevious.intValue() != averageLossNew.intValue() || averageLossNew.intValue() == Integer.MAX_VALUE || averageLossNew.intValue() == Integer.MIN_VALUE || averageLossPrevious.intValue() == Integer.MAX_VALUE || averageLossPrevious.intValue() == Integer.MIN_VALUE) {
-            epochs++;
-            System.out.println("ALN: " + averageLossNew.intValue() + " | ALP: " + averageLossPrevious.intValue());
-            // if average loss is flip flopping between being positive and negative or getting larger, reduce learning rate
-            if ((averageLossNew > 0.0 && averageLossPrevious < 0.0) || (averageLossNew < 0.0 && averageLossPrevious > 0.0) || ((Math.pow(averageLossNew, 2) >= Math.pow(averageLossPrevious, 2)))) {
-                lr = lr * lrReductionRate;
-                System.out.println("Learning rate reduced to " + lr);
-            }
-
-            averageLossPrevious = averageLossNew;
-
-            // Compute loss for each feature and adjust model weight accordingly
-            for (int i = 0; i < numOfFeatures; i++) {
-                ArrayList<Double> current = dataset.getSingleFeatureDataset(i);
-                Double totalLoss = 0.0;
-                for (int j = 0; j < dataset.getNumOfDatapoints(); j++) {
-                    Double holder = 0.0;
-                    Double coefficient = (-2.0) * current.get(j);
-                    Double label = dataset.getLabels().get(j);
-                    for (int f = 0; f < numOfFeatures; f++) {
-                        ArrayList<Double> weightAdjCurrent = dataset.getSingleFeatureDataset(f);
-                        holder += (model.getWeights().get(f) * weightAdjCurrent.get(j));
-                    }
-                    holder += model.getBias();
-                    totalLoss += coefficient * (label - holder);
-                }
-                Double lossAdj = totalLoss / dataset.getNumOfDatapoints();
-                // Adjust model weight
-                model.setWeight(i, model.getWeights().get(i) - (lr * lossAdj));
-            }
-
-            // perform same process as above, but for model bias
-            Double totalLossB = 0.0;
-            for (int i = 0; i < dataset.getNumOfDatapoints(); i++) {
-                Double holder = 0.0;
-                Double coefficient = (-2.0);
-                Double label = dataset.getLabels().get(i);
-                for (int j = 0; j < numOfFeatures; j++) {
-                    ArrayList<Double> current = dataset.getSingleFeatureDataset(j);
-                    holder += (model.getWeights().get(j) * current.get(i));
-                }
-                holder += model.getBias();
-                totalLossB += coefficient * (label - holder);
-            }
-            Double lossAdjB = totalLossB / dataset.getNumOfDatapoints();
-            model.setBias(model.getBias() - (lr * lossAdjB));
-
-            System.out.print("Weights n Bias: ");
-            for (int i = 0; i < model.getWeights().size(); i++) {
-                System.out.print("w" + i + ":" + model.getWeights().get(i) + " ");
-            }
-            System.out.println("b:" + model.getBias());
-
-            // compute new average loss
-            Double averageLoss = 0.0;
-            for (int i = 0; i < dataset.getNumOfDatapoints(); i++) {
-                Double predictedValue = model.getBias();
-                for (int j = 0; j < numOfFeatures; j++) {
-                    ArrayList<Double> current = dataset.getSingleFeatureDataset(j);
-                    predictedValue += (model.getWeights().get(j) * current.get(i));
-                }
-                averageLoss += dataset.getLabels().get(i) - predictedValue;
-            }
-            averageLoss = averageLoss / dataset.getNumOfDatapoints();
-
-            averageLossNew = averageLoss;
-            System.out.println("Average Loss: " + averageLoss);
-        }
         
-        System.out.println("ALN: " + averageLossNew.intValue() + " | ALP: " + averageLossPrevious.intValue());
-        System.out.println("Epochs: " + epochs);
+
         return model;
     }
     
