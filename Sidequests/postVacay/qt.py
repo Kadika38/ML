822a907ed4187ac42bfc6a50b0adb6a584022629
David <dryan10101@gmail.com>
2023-07-25 13:49:55 -0700

    Continued to work in Sidequests/postVacay

--- Sidequests/postVacay/qt.py
+++ Sidequests/postVacay/qt.py
@@ -3,33 +3,25 @@ import requests
 from matplotlib import pyplot as plt
 import pandas as pd
 
-stocks = ["AAPL", "ADPT", "AMC", "AMD", "BA", "CRSP", "LUV", "SBUX", "SPCE", "TSLA"]
-stocksTest = ["AAPL"]
-
-for stock in stocksTest:
-    response = requests.get("https://api.twelvedata.com/time_series?apikey=c3efaf8bc4d14828a7574cf215662e7f&interval=1day&format=JSON&symbol=" + stock + "&previous_close=true&outputsize=1000")
-    json = response.json()
-    originalDF = pd.DataFrame(json['values'])
-    originalDF["pcVector"] = 100.0 * ((originalDF['close'].astype(float) - originalDF['previous_close'].astype(float)) / originalDF["previous_close"].astype(float))
-    
+def createPatternResultDF(df, ps, rs):
     patternsArray = []
     resultsArray = []
 
     
-    for index, row in originalDF.iterrows():
-        print("Row " + str(index) + " of " + str(len(originalDF.index)))
-        for patternSize in range(2, 15):
-            for resultSize in range(1, 15):
-                if (len(originalDF.index) > (index + patternSize + resultSize -1)):
+    for index, row in df.iterrows():
+        # print("Row " + str(index) + " of " + str(len(df.index)))
+        for patternSize in range(ps, ps+1):
+            for resultSize in range(rs, rs+1):
+                if (len(df.index) > (index + patternSize + resultSize -1)):
                     pattern = []
                     result = []
                     for i in range(patternSize):
-                        if (originalDF.iloc[index + i]['pcVector'] > 0.0):
+                        if (df.iloc[index + i]['pcVector'] > 0.0):
                             pattern.append(True)
                         else:
                             pattern.append(False)
                     for i in range(resultSize):
-                        if (originalDF.iloc[index + patternSize + i]['pcVector'] > 0.0):
+                        if (df.iloc[index + patternSize + i]['pcVector'] > 0.0):
                             result.append(True)
                         else:
                             result.append(False)
@@ -65,7 +57,48 @@ for stock in stocksTest:
         pe = highest * 100.0
 
         newDF.loc[len(newDF.index)] = [pa, r, rn, pe]
-    
-    newDF.sort_values(['Percentage'])
 
-    newDF.to_csv("Sidequests/postVacay/qt.txt")
+    return newDF.sort_values('Percentage')
+
+stocks = ["AAPL", "ADPT", "AMC", "AMD", "BA", "CRSP", "LUV", "SBUX", "SPCE", "TSLA"]
+stocksTest = ["AAPL"]
+
+finalDF = pd.DataFrame()
+finalDF['Stock'] = []
+finalDF['Pattern'] = []
+finalDF['Result'] = []
+finalDF['ResultNum'] = []
+finalDF['Percentage'] = []
+
+for stock in stocks:
+    response = requests.get("https://api.twelvedata.com/time_series?apikey=c3efaf8bc4d14828a7574cf215662e7f&interval=1day&format=JSON&symbol=" + stock + "&previous_close=true&outputsize=1000")
+    json = response.json()
+    originalDF = pd.DataFrame(json['values'])
+    originalDF["pcVector"] = 100.0 * ((originalDF['close'].astype(float) - originalDF['previous_close'].astype(float)) / originalDF["previous_close"].astype(float))
+
+    mostRecentPatterns = []
+    for r in range(2, 15):
+        pattern = []
+        for i in range(r):
+            if (originalDF.iloc[i]['pcVector'] > 0.0):
+                pattern.insert(0, True)
+            else:
+                pattern.insert(0, False)
+        mostRecentPatterns.append(pattern)
+    print(mostRecentPatterns)
+
+    allDataFrames = []
+
+    for ps in range(2, 15):
+        for rs in range(1,3):
+            print("Pattern Size: " + str(ps) + ", Result Size: " + str(rs))
+            allDataFrames.append(createPatternResultDF(originalDF, ps, rs))
+
+    for df in allDataFrames:
+        for index in range(len(df.index)):
+            if (mostRecentPatterns.count(df.iloc[index]['Pattern']) > 0):
+                if (df.iloc[index]['ResultNum'] > 9 and df.iloc[index]['Percentage'] > 60.0):
+                    finalDF.loc[len(finalDF.index)] = [stock, df.iloc[index]['Pattern'], df.iloc[index]['Result'], df.iloc[index]['ResultNum'], df.iloc[index]['Percentage']]
+    
+
+finalDF.to_csv('Sidequests/postVacay/qt.csv')
